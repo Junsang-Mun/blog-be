94279eb92ee5bd7abc177e146815796b54c4b9d3
Junsang Mun <mun.js@aol.com>
2023-03-11 20:42:42 +0900

    [add] server.ts file

---
+++ server.ts
@@ -0,0 +1,21 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import postController from "./controllers/post.ts"
+
+const router = new Router();
+router
+  .get('/api/posts', postController.getAllPosts)
+  .post('/api/posts', postController.createPost)
+  .get('/api/posts/:id', postController.getPostById)
+  .put('/api/posts/:id', postController.updatePostById)
+  .delete('/api/posts/:id', postController.deletePostById);
+
+const app = new Application();
+const port = 8080;
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+  "listen",
+  (_e) => console.log(`Listening on http://localhost:${port}`),
+);
+await app.listen({ port: port });
