4b03ff5b5745577f5f6d27c13b03e188e4bff178
Junsang Mun <mun.js@aol.com>
2023-03-11 23:09:46 +0900

    [add] add mock data

---
+++ mocks/post.ts
@@ -0,0 +1,24 @@
+import Post from "../interfaces/Post.ts";
+
+const posts: Post[] = [
+	{
+		id: self.crypto.randomUUID(),
+		title: "First Post",
+		body: "<p>asdfasdflaskdjflkasjdlf</p>",
+		date: "2022-01-01",
+	},
+	{
+		id: self.crypto.randomUUID(),
+		title: "Second Post",
+		body: "<p>192384792483749283759860</p>",
+		date: "2022-01-02",
+	},
+	{
+		id: self.crypto.randomUUID(),
+		title: "Third Post",
+		body: "<p>plplmplmplmplmplmplmplmplm</p>",
+		date: "2022-01-03",
+	}
+]
+
+export default posts;
