ef47ca6b23266b2282406c3401fc00a78559334a
Junsang Mun <mun.js@aol.com>
2023-03-12 13:12:45 +0900

    [add] add controllers
    - [end] getAllPosts
    - [end] createPost
    - [WIP] getPostById
    - [TBD] updatePostById
    - [TBD] deletePostById

--- controllers/post.ts
+++ controllers/post.ts
@@ -1,7 +1,56 @@
+import { Context, RouterContext } from "https://deno.land/x/oak/mod.ts";
+import posts from "../mocks/post.ts";
+import Post from "../interfaces/Post.ts";
+
 export default {
-	getAllPosts: () => {},
-	createPost: () => {},
-	getPostById: () => {},
+	getAllPosts: ({ response }: Context) => {
+		response.status = 200;
+		response.body = {
+			success: true,
+			data: posts,
+		}
+	},
+	createPost: async ( ctx: Context, ) => {
+		const { value } = ctx.request.body({ type: 'json' });
+		const { title, body, date } = await value;
+		if (!title || !body || !date) {
+			ctx.response.status = 400;
+			ctx.response.body = {
+				success: false,
+				message: "No data provided",
+			};
+			return;
+		}
+		const newPost: Post = {
+			id: self.crypto.randomUUID(),
+			title: title,
+			body: body,
+			date: date,
+		};
+		const data = [...posts, newPost];
+		ctx.response.body = {
+			success: true,
+			data,
+		};
+	},
+	getPostById: (ctx: RouterContext) => {
+		const post: Post | undefined = posts.find((t) => {
+			return t.id === params.id;
+		});
+		if (!post) {
+			response.status = 404;
+			response.body = {
+				success: false,
+				message: "No such post",
+			};
+			return;
+		}
+		response.status = 200;
+		response.body = {
+			success: true,
+			data: post,
+		};
+	},
 	updatePostById: () => {},
 	deletePostById: () => {},
 }
